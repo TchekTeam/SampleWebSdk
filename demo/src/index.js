50bd5ed2ca0f9a19376cac3d03d619c1b43306ea
Saïd Mezhoud <said.mezhoud@tchek.ai>
2022-05-11 17:58:02 +0200

    add https to parcel runner, add permissions to iframe to index.html, add some logs on window iframe message div block

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,6 +1,10 @@
+import "./styles.css";
+
 document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("message", function (e) {
     const data = JSON.parse(e.data);
+    const date = new Date(data.date).toLocaleTimeString("en-US");
+    messageEle.innerHTML = `Receive "${data.message}" at ${date}<br>` + messageEle.innerHTML;
     console.log(data);
   });
 });
